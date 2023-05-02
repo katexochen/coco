e80737265017d888680732316023d8e31c5df7ad
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-05-02 18:50:02 +0200

    Basic terraform for AKS CC
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ azure/.terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.54.0"
+  constraints = "3.54.0"
+  hashes = [
+    "h1:56VLqEh+sGU/PLO72UDPKOke3whNNjm4snQQwMzne28=",
+    "zh:0b5c5ffecc73c0f24f6c06c73883153affcb69a0a05376c4a34d71566be7c1e0",
+    "zh:0fb685212f914856c01b7182e2669b9b6d7a3cb77578e2d789baaab3e58fc63e",
+    "zh:3d938b865b36f4821a97e70dbbf8a22f498fb4e9848012c62e8ff2c1461d0760",
+    "zh:42d506add9bba2e96780c0b406f2d6020cf042ffae96faf96988a959766b3644",
+    "zh:433b105444462528f3b3e8619a2edbef6dbd5ce0c694f37ab86d91540c1e427a",
+    "zh:66c3cffd8b01413914703b1728d378e6d37d3b929e84c2a875ad125b828ce1d4",
+    "zh:7cb7187bfcca6f2d4e9d32544b2ca678b65c7c9f520b402d04301152be0c0aa3",
+    "zh:accf83e742d7edfe920b62523a190180257cd59180d0ec0435bf09b74a18e0ae",
+    "zh:c07b4d169931852007b2ba0ad6aaa24e10e9e3b17731080002ef927fb9912483",
+    "zh:e43d945e51228dfb0e1032590fb9409b2b89c40ec8a29cfd2efbd51c536a75a4",
+    "zh:ee248b8cf65fa9d3e8d3859cfcdc6940de5806bdc181b1a794085de162cceb7e",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.1.0"
+  constraints = "2.1.0"
+  hashes = [
+    "h1:EYZdckuGU3n6APs97nS2LxZm3dDtGqyM4qaIvsmac8o=",
+    "zh:0f1ec65101fa35050978d483d6e8916664b7556800348456ff3d09454ac1eae2",
+    "zh:36e42ac19f5d68467aacf07e6adcf83c7486f2e5b5f4339e9671f68525fc87ab",
+    "zh:6db9db2a1819e77b1642ec3b5e95042b202aee8151a0256d289f2e141bf3ceb3",
+    "zh:719dfd97bb9ddce99f7d741260b8ece2682b363735c764cac83303f02386075a",
+    "zh:7598bb86e0378fd97eaa04638c1a4c75f960f62f69d3662e6d80ffa5a89847fe",
+    "zh:ad0a188b52517fec9eca393f1e2c9daea362b33ae2eb38a857b6b09949a727c1",
+    "zh:c46846c8df66a13fee6eff7dc5d528a7f868ae0dcf92d79deaac73cc297ed20c",
+    "zh:dc1a20a2eec12095d04bf6da5321f535351a594a636912361db20eb2a707ccc4",
+    "zh:e57ab4771a9d999401f6badd8b018558357d3cbdf3d33cc0c4f83e818ca8e94b",
+    "zh:ebdcde208072b4b0f8d305ebf2bfdc62c926e0717599dcf8ec2fd8c5845031c3",
+    "zh:ef34c52b68933bedd0868a13ccfd59ff1c820f299760b3c02e008dc95e2ece91",
+  ]
+}
---
+++ azure/main.tf
@@ -0,0 +1,119 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.54.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "enable_sgx_pool" {
+  type = bool
+}
+
+variable "enable_snp_pool" {
+  type = bool
+}
+
+variable "enable_kata" {
+  type = bool
+}
+
+locals {
+  name = "${var.name_prefix}-aks"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.name
+  location = "UK South"
+}
+
+#
+# Compare aks setup at
+# https://github.com/tkubica12/azure-workshops/blob/6fb761a26cc91a72be41e562ccedc140dd1be392/d-data-security/terraform/aks.tf
+#
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = local.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  dns_prefix          = local.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  confidential_computing {
+    sgx_quote_helper_enabled = true
+  }
+
+  default_node_pool {
+    name                = "defaultpool"
+    node_count          = 1
+    vm_size             = "Standard_B2ms"
+    enable_auto_scaling = false
+    type                = "VirtualMachineScaleSets"
+  }
+}
+
+// Confidential nodepool
+resource "azurerm_kubernetes_cluster_node_pool" "confidential_node" {
+  count                 = var.enable_snp_pool ? 1 : 0
+  name                  = "sevsnp"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+  vm_size               = "Standard_DC2as_v5"
+  node_count            = 1
+  zones                 = ["2"]
+
+  node_labels = {
+    securetype = "sevsnp"
+  }
+}
+
+// Nodepool supporting SXG confidential containers
+resource "azurerm_kubernetes_cluster_node_pool" "confidential_sxg_containers" {
+  count                 = var.enable_sgx_pool ? 1 : 0
+  name                  = "sgx"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+  vm_size               = "Standard_DC2s_v3"
+  node_count            = 1
+  zones                 = ["1"]
+
+  node_labels = {
+    securetype = "sgx"
+  }
+}
+
+// Nodepool supporting nested virtualization kata containers
+resource "azurerm_kubernetes_cluster_node_pool" "kata" {
+  count                 = var.enable_kata ? 1 : 0
+  name                  = "kata"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+  vm_size               = "Standard_E2s_v3"
+  node_count            = 1
+  zones                 = ["1"]
+  workload_runtime      = "KataMshvVmIsolation"
+  os_sku                = "Mariner"
+
+  node_labels = {
+    securetype = "kata"
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.cluster]
+  filename   = "./kube.conf"
+  content    = azurerm_kubernetes_cluster.cluster.kube_config_raw
+}
